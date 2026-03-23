6ae07adcc7e20a26b7495d88f53c359783014154
SanketMole <sanketmole78@gmail.com>
2026-03-23 11:41:51 +0530

    String Palindrome problem using Python.

---
+++ DSA/String Palindrome.py
@@ -0,0 +1,17 @@
+def palindrome(s):
+    left, right = 0, len(s)-1
+    while(left<right):
+        if not s[left].isalnum():
+            left+=1
+        elif not s[right].isalnum():
+            right-=1
+        elif s[left].lower() != s[right].lower():
+            return False
+        else:
+            left += 1
+            right -=1
+    return True
+
+if __name__ == "__main__":
+    s = "A man, a plan, a canal: Panama"
+    print(palindrome(s))
